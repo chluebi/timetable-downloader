0a4260361ace7f4c2094ff2348d4171e78ae6121
Unknown <ninjago@sunrise.ch>
2019-08-17 13:46:56 +0200

    made main.py runnable by itself

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import sel
 import cut
+import json
+from PIL import Image
+import asyncio
 
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
 async def make_timetable(config):
     sel.path = config['selenium']['path-to-gecko']
     w = config['website']
 
     print('--- logging into the website ---')
-    sel.get_timetable(w['url'], w['username'], w['password'], '2e')
+    sel.get_timetable(w['url'], w['username'], w['password'], '3a')
     print('--- took screenshot ---')
     cut.cut_pic('timetable.png', 0, 270, 1450, 1200)
     print('--- successfully cropped ---')
 
 
 if __name__ == '__main__':
-    make_timetable()
+    asyncio.run(make_timetable(config))
+    im = Image.open('cut_timetable.png')
+    im.show()
